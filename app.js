943387ab6ff08d788468b5ad419699e2f80f12c8
Brian Shin <39751822+iambrianshin@users.noreply.github.com>
2019-01-05 23:57:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-var budgetController = (function() {
-  var x = 23;
+// BUDGET CONTROLLER
+var budgetController = (function() {})();
 
-  var add = function(a) {
-    return x + a;
-  };
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  };
-})();
-
+// UI CONTROLLER
 var UIController = (function() {
   // Some Code
 })();
 
-var controller = (function(budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5);
+// GLOBAL APP CONTROLLER
 
-  return {
-    anotherPublic: function() {
-      console.log(z);
-    }
+var controller = (function(budgetCtrl, UICtrl) {
+  var ctrlAddItem = function() {
+    // 1. Get the filed input data
+
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+
+    console.log("It works");
   };
+
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+
+  document.addEventListener("keypress", function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
 })(budgetController, UIController);
